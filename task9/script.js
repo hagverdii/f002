6e7c4a4bd1baff85a16a4df5527b890ea5305eb6
Mammad Hagverdi <memmed.haqverdi02@gmail.com>
2025-10-12 18:52:19 +0400

    update

--- task9/script.js
+++ task9/script.js
@@ -1,42 +1,27 @@
-let sliderLeftButton = document.getElementById("slider-left-button");
-let sliderRightButton = document.getElementById("slider-right-button");
-let sliderImage = document.getElementById("slider-image");
-let sliderRadioButtons = document.querySelector(".slider-radio-buttons");
+const sliderImagesContainer = document.querySelector(".slider-images");
+const sliderLeftButton = document.getElementById("slider-left-button");
+const sliderRightButton = document.getElementById("slider-right-button");
+const sliderRadioButtons = document.querySelector(".slider-radio-buttons");
 
-let imageSources = [
+const imageSources = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9APxkj0xClmrU3PpMZglHQkx446nQPG6lA&s",
   "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg",
   "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
 ];
 
-let sliderCounter = 0;
+let currentIndex = 0;
 
-sliderImage.setAttribute("src", imageSources[sliderCounter]);
+imageSources.forEach((src) => {
+  const img = document.createElement("img");
+  img.src = src;
+  sliderImagesContainer.appendChild(img);
+});
 
-for (let i = 0; i < imageSources.length; i++) {
-  let radioButton = document.createElement("button");
-  radioButton.textContent = "*";
-  radioButton.addEventListener("click", () => {
-    sliderCounter = i;
-    sliderImage.setAttribute("src", imageSources[sliderCounter]);
-  });
-  sliderRadioButtons.appendChild(radioButton);
+function goToSlide(index) {
+  currentIndex = (index + imageSources.length) % imageSources.length;
+  sliderImagesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+  updateDots();
 }
 
-sliderLeftButton.addEventListener("click", () => {
-  if (sliderCounter > 0) {
-    sliderImage.setAttribute("src", imageSources[--sliderCounter]);
-  } else {
-    sliderCounter = imageSources.length - 1;
-    sliderImage.setAttribute("src", imageSources[sliderCounter]);
-  }
-});
-
-sliderRightButton.addEventListener("click", () => {
-  if (sliderCounter < imageSources.length - 1) {
-    sliderImage.setAttribute("src", imageSources[++sliderCounter]);
-  } else {
-    sliderCounter = 0;
-    sliderImage.setAttribute("src", imageSources[sliderCounter]);
-  }
-});
+sliderLeftButton.addEventListener("click", () => goToSlide(currentIndex - 1));
+sliderRightButton.addEventListener("click", () => goToSlide(currentIndex + 1));
